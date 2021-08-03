ac42cd4e19cd323b0393863a16358a25d3428678
Nicole Bruneau <nicole_bruneau@hotmail.com>
2021-08-02 21:11:08 -0500

    allProfs

--- Directivas.c
+++ Directivas.c
@@ -1,8 +1,37 @@
+/**#include <stdio.h>
+
 #define M 5
 #define C 5
 
 int main(int argc, char* argv[]) {
     int x = 2;
     int y = x*M + C;
+    printf("%d\n",y);
+    return 0;
+}
+
+
+#include <stdio.h>
+#define M 5
+#define C 5
+#define MAP(val,m,c) x*m+c
+
+int main(int argc, char* argv[]) {
+    int x = 2;
+    int y = MAP(x,M,C);
+    printf("y: %d\n", y);
+    return 0;
+}*/
+
+#include <stdio.h>
+
+
+int main(int argc, char* argv[]) {
+
+    #ifdef CONDITION
+    printf("CODIGO IF\n");
+    #else
+    printf("CODIGO ELSE\n");
+    #endif
     return 0;
 }
